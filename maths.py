6c509402c1d14d751bb33441cf322d0afd406b64
200727379 <rampedilesley@gmail.com>
2021-04-27 11:49:43 -0700

    Initial commit

---
+++ maths.py
@@ -0,0 +1,13 @@
+#Author: Mothapo Rampedi Lesley
+#27 April 2021 (Freedom Day)
+"""Write a program that requests the user to enter two numbers and prints the sum, product, difference
+and quotient of the two numbers.
+"""
+
+number_1 = int(input("Enter the first number:  "))
+number_2 = int(input("Enter the second number:  "))
+
+print("The sum is ", number_1 + number_2)
+print("The product is ", number_1 * number_2)
+print("The difference is ", number_1 - number_2)
+print("The quotient is ", number_1 / number_2)
